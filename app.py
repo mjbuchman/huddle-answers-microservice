91d75a0285c6a4c57de9b82022773e06fbb93cd3
mjbuchman <mbookie213@gmail.com>
2022-04-04 21:20:10 -0400

    rollover

--- app.py
+++ app.py
@@ -7,37 +7,41 @@ app = Flask(__name__)
 CORS(app)
 
 def determineAnswer():
-	f = open("./data/todaysAnswer.json", "w")
 	dateSeed = date.today()
 	random.seed(dateSeed)
 	with open('./data/players.json') as json_data:
 		players = json.load(json_data)
 		index = random.randint(0,len(players)-1)
-		f.write(json.dumps(players[index]))
-	f.close()
-
-def getPuzzleId():
-    startDate = date(2022,4,4) #YYYY-mm-dd
-    id = date.today() - startDate
-    return id.seconds
-
+		return players[index]
+  
+def rolloverDay():
+	with open('./data/todaysAnswer.json', "r") as json_data:
+		todaysAnswer = json.load(json_data)
+		jsonString = {"answer": determineAnswer(), "puzzleId": todaysAnswer["puzzleId"] + 1}
+	with open('./data/todaysAnswer.json', "w") as json_data:
+		json_data.write(json.dumps(jsonString, indent=3))
+    
 @app.route('/', methods=['GET', 'POST'])
 def index():
 	with open('./data/todaysAnswer.json') as json_data:
 		todaysAnswer = json.load(json_data)
-	return render_template("index.html", answer=todaysAnswer, puzzleId=getPuzzleId())
+	return render_template("index.html", answer=todaysAnswer["answer"], puzzleId=todaysAnswer["puzzleId"])
 
 @app.route('/dailyAnswer')
 def answer():
-	response = jsonify({"answer": determineAnswer()})
-	response.headers.add('Access-Control-Allow-Origin', '*')
-	return response
+	with open('./data/todaysAnswer.json', "r") as json_data:
+		todaysAnswer = json.load(json_data)
+		response = jsonify({"answer": todaysAnswer["answer"]})
+		response.headers.add('Access-Control-Allow-Origin', '*')
+		return response
 
 @app.route('/dailyPuzzleId')
 def puzzleId():
-	response = jsonify({"puzzleId": getPuzzleId()})
-	response.headers.add('Access-Control-Allow-Origin', '*')
-	return response
+	with open('./data/todaysAnswer.json', "r") as json_data:
+		todaysAnswer = json.load(json_data)
+		response = jsonify({"answer": todaysAnswer["puzzleId"]})
+		response.headers.add('Access-Control-Allow-Origin', '*')
+		return response
 
 if __name__ == '__main__':
     app.run(debug=True)
