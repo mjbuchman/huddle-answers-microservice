6567d4463f3210299f46f5ac941722ea471b7861
mjbuchman <mbookie213@gmail.com>
2022-04-05 09:32:50 -0400

    json stored remote

--- app.py
+++ app.py
@@ -1,31 +1,35 @@
 from flask import Flask, jsonify, render_template
 from flask_cors import CORS
-import json
+from dotenv import load_dotenv 
+import json, requests, os
 
 app = Flask(__name__)
 CORS(app)
+load_dotenv()
     
+answerBin = os.environ.get("answerBin")
+masterKey = os.environ.get("masterKey")
+
+def getFile():
+	url = f'https://api.jsonbin.io/v3/b/{answerBin}'
+	headers = {
+		'X-Master-Key': masterKey
+	}
+	return requests.get(url, json=None, headers=headers)
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
-	with open('./data/todaysAnswer.json') as json_data:
-		todaysAnswer = json.load(json_data)
-	return render_template("index.html", answer=todaysAnswer["answer"], puzzleId=todaysAnswer["puzzleId"])
+	json_data = getFile()
+	todaysAnswer = json.loads(json_data.text)
+	return render_template("index.html", answer=todaysAnswer["record"]["answer"], puzzleId=todaysAnswer["record"]["puzzleId"])
 
 @app.route('/dailyAnswer')
 def answer():
-	with open('./data/todaysAnswer.json', "r") as json_data:
-		todaysAnswer = json.load(json_data)
-		response = jsonify({"answer": todaysAnswer["answer"]})
-		response.headers.add('Access-Control-Allow-Origin', '*')
-		return response
-
-@app.route('/dailyPuzzleId')
-def puzzleId():
-	with open('./data/todaysAnswer.json', "r") as json_data:
-		todaysAnswer = json.load(json_data)
-		response = jsonify({"answer": todaysAnswer["puzzleId"]})
-		response.headers.add('Access-Control-Allow-Origin', '*')
-		return response
+	json_data = getFile()
+	todaysAnswer = json.loads(json_data.text)
+	response = jsonify({"answer": todaysAnswer["record"]})
+	response.headers.add('Access-Control-Allow-Origin', '*')
+	return response
 
 if __name__ == '__main__':
     app.run()
--- rolloverDay.py
+++ rolloverDay.py
@@ -1,20 +1,53 @@
-from datetime import date
-import json,random
+from dotenv import load_dotenv 
+from datetime import datetime
+import json, random, requests, os
+
+load_dotenv()
+
+playersBin = os.environ.get("playersBin")
+usedBin = os.environ.get("usedBin")
+answerBin = os.environ.get("answerBin")
+masterKey = os.environ.get("masterKey")
+
+def getFile(bin):
+	url = f'https://api.jsonbin.io/v3/b/{bin}'
+	headers = {
+		'X-Master-Key': masterKey
+	}
+	return requests.get(url, json=None, headers=headers)
+
+def updateFile(bin, data):
+	url = f'https://api.jsonbin.io/v3/b/{bin}'
+	headers = {
+		'Content-Type': 'application/json',
+		'X-Master-Key': masterKey
+	}
+	return requests.put(url, json=data, headers=headers)
+
+def addToUsedAndDelete(answer, players, index):
+	json_data = getFile(usedBin)
+	used = json.loads(json_data.text)
+	used["record"].append(answer["Name"])
+	updateFile(usedBin, used["record"])
+ 
+	del players["record"][index]
+	updateFile(playersBin, players["record"])
 
 def determineAnswer():
-	dateSeed = date.today().strftime("%m/%d/%Y, %H:%M:%S")
-	random.seed(dateSeed)
-	with open('./data/players.json') as json_data:
-		players = json.load(json_data)
-		index = random.randint(0,len(players)-1)
-		return players[index]
+	dateSeed = datetime.now().strftime("%m%d%Y%H%M%S")
+	random.seed(int(dateSeed))
+	json_data = getFile(playersBin)
+	players = json.loads(json_data.text)
+	index = random.randint(0,len(players["record"])-1)
+	answer = players["record"][index]
+	addToUsedAndDelete(answer, players, index)
+	return answer
   
 def rolloverDay():
-	with open('./data/todaysAnswer.json', "r") as json_data:
-		todaysAnswer = json.load(json_data)
-		jsonString = {"answer": determineAnswer(), "puzzleId": todaysAnswer["puzzleId"] + 1}
-	with open('./data/todaysAnswer.json', "w") as json_data:
-		json_data.write(json.dumps(jsonString, indent=3))
+	json_data = getFile(answerBin)
+	todaysAnswer = json.loads(json_data.text)
+	jsonString = {"answer": determineAnswer(), "puzzleId": todaysAnswer["record"]["puzzleId"] + 1}
+	updateFile(answerBin, jsonString)
   
 if __name__ == '__main__':
     rolloverDay()
